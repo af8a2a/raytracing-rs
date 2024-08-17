8dd9588fa6db4d76e03f312612e9a6863d34cff2
af8a2a <1125209282@qq.com>
2024-08-17 23:44:13 +0800

    add:sphere hit fix:draw code

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ use nalgebra::Vector3;
 use pbrt_rs::ray::Ray;
 
 fn ray_color(ray: &Ray) -> Vector3<f32> {
+    if hit_sphere(Vector3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        return Vector3::new(1.0, 0.0, 0.0);
+    }
     let unit_dir = ray.direction.normalize();
     let t = 0.5 * (unit_dir.y + 1.0);
     (1.0 - t) * Vector3::new(1.0, 1.0, 1.0) + t * Vector3::new(0.5, 0.7, 1.0)
@@ -16,24 +19,34 @@ fn color_to_rgb(color: Vector3<f32>) -> Rgb<u8> {
     ])
 }
 
+fn hit_sphere(center: Vector3<f32>, radius: f32, ray: &Ray) -> bool {
+    let oc = center - ray.origin;
+    let a = ray.direction.dot(&ray.direction);
+    let b = -2.0 * oc.dot(&ray.direction);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
+}
+
 fn main() {
     let aspect_ratio: f32 = 16.0 / 9.0;
+
     let width = 400.0;
-    let height = width / aspect_ratio;
+    let height = (width / aspect_ratio).floor();
 
     let focal_length = 1.0;
     let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let origin = Vector3::new(0.0, 0.0, 0.0);
+    let viewport_width = viewport_height * width / height;
+    let camera_center = Vector3::new(0.0, 0.0, 0.0);
 
     let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vector3::new(0.0, viewport_height, 0.0);
+    let viewport_v = Vector3::new(0.0, -viewport_height, 0.0);
 
     let pixel_delta_u = viewport_u / width;
     let pixel_delta_v = viewport_v / height;
 
     let viewport_upper_left =
-        origin - Vector3::new(0.0, 0.0, focal_length) - pixel_delta_u / 2.0 - pixel_delta_v / 2.0;
+        camera_center - Vector3::new(0.0, 0.0, focal_length) - viewport_u / 2.0 - viewport_v / 2.0;
 
     let pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
@@ -43,8 +56,8 @@ fn main() {
             let pixel_center =
                 pixel100_loc + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
 
-            let ray_dir = pixel_center - origin;
-            let ray = Ray::new(origin, ray_dir);
+            let ray_dir = pixel_center - camera_center;
+            let ray = Ray::new(camera_center, ray_dir);
             let color = ray_color(&ray);
             image.put_pixel(i, j, color_to_rgb(color));
         }
