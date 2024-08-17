d21d19876417a43dcf6b163794fb4d4f2c5ac2cf
af8a2a <1125209282@qq.com>
2024-08-17 23:26:15 +0800

    Add:Ray

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,52 @@
 use image::{Pixel, Rgb, RgbImage};
+use nalgebra::Vector3;
+use pbrt_rs::ray::Ray;
+
+fn ray_color(ray: &Ray) -> Vector3<f32> {
+    let unit_dir = ray.direction.normalize();
+    let t = 0.5 * (unit_dir.y + 1.0);
+    (1.0 - t) * Vector3::new(1.0, 1.0, 1.0) + t * Vector3::new(0.5, 0.7, 1.0)
+}
+
+fn color_to_rgb(color: Vector3<f32>) -> Rgb<u8> {
+    Rgb([
+        (255.0 * color.x) as u8,
+        (255.0 * color.y) as u8,
+        (255.0 * color.z) as u8,
+    ])
+}
 
 fn main() {
-    let (width, height) = (800, 600);
-    let mut image = RgbImage::new(width, height);
-    for j in 0..height {
-        for i in 0..width {
-            image.put_pixel(
-                i,
-                j,
-                Rgb([
-                    (255.0 * i as f32 / (width - 1) as f32) as u8,
-                    (255.0 * j as f32 / (height - 1) as f32) as u8,
-                    0,
-                ]),
-            );
+    let aspect_ratio: f32 = 16.0 / 9.0;
+    let width = 400.0;
+    let height = width / aspect_ratio;
+
+    let focal_length = 1.0;
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let origin = Vector3::new(0.0, 0.0, 0.0);
+
+    let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vector3::new(0.0, viewport_height, 0.0);
+
+    let pixel_delta_u = viewport_u / width;
+    let pixel_delta_v = viewport_v / height;
+
+    let viewport_upper_left =
+        origin - Vector3::new(0.0, 0.0, focal_length) - pixel_delta_u / 2.0 - pixel_delta_v / 2.0;
+
+    let pixel100_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+    let mut image = RgbImage::new(width as u32, height as u32);
+    for j in 0..height as u32 {
+        for i in 0..width as u32 {
+            let pixel_center =
+                pixel100_loc + (i as f32 * pixel_delta_u) + (j as f32 * pixel_delta_v);
+
+            let ray_dir = pixel_center - origin;
+            let ray = Ray::new(origin, ray_dir);
+            let color = ray_color(&ray);
+            image.put_pixel(i, j, color_to_rgb(color));
         }
     }
     image.save("image.png").expect("Failed to save image");
---
+++ src/ray.rs
@@ -0,0 +1,15 @@
+use nalgebra::Vector3;
+
+pub struct Ray {
+    pub origin: Vector3<f32>,
+    pub direction: Vector3<f32>,
+}
+
+impl Ray {
+    pub fn new(origin: Vector3<f32>, direction: Vector3<f32>) -> Self {
+        Self { origin, direction }
+    }
+    pub fn at(&self, t: f32) -> Vector3<f32> {
+        self.origin + t * self.direction
+    }
+}
